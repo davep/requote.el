744049a2cf9d79d8d0965cc2480bacaa1543ab94
Dave Pearson <davep@davep.org>
2018-05-10 20:52:31 +0100

    :bookmark: v1.0

--- requote.el
+++ requote.el
@@ -2,7 +2,7 @@
 ;; Copyright 2018 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 0.01
+;; Version: 1.0
 ;; Keywords: convenience, string, quotes
 ;; URL: https://github.com/davep/requote.el
 ;; Package-Requires: ((emacs "24"))
